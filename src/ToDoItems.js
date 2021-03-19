89fbd5a94361ff24be20d41f33f018a393788176
Queenie Ho <qho.queenieho@gmail.com>
2021-03-19 15:13:24 -0700

    now the task and date columns are sortable both ascendingly and descendingly

--- src/ToDoItems.js
+++ src/ToDoItems.js
@@ -2,22 +2,10 @@ import React from 'react'
 import EachItem from './EachItem';
 
 export default function ToDoItems({ todos, toggleCompleted }) {
-    const preSortedItems = todos;
-    let sortedItems = [...preSortedItems];
-
-    sortedItems.sort((a,b) => {
-        if(+(new Date(a.due)) < +(new Date(b.due))) return -1;
-        if(+(new Date(a.due)) > +(new Date(b.due))) return 1;
-        return 0;
-    });
-
     return (
-        
-        sortedItems.map(eachItem => {
+        todos.map(eachItem => {
             return (
-
                 <EachItem key={eachItem.id} eachItem={eachItem} toggleCompleted={toggleCompleted} />
-
             )
         })
     )
--- src/index.js
+++ src/index.js
@@ -5,22 +5,24 @@ import { v4 as uuidv4 } from 'uuid';
 import ToDoItems from './ToDoItems';
 import './index.css';
 
-const LOCAL_STORAGE_KEY = 'todoApp.todos';
+const LOCAL_STORAGE_KEY_DATA = 'todoApp.todos';
+
 const tomrISO = new Date().toISOString().split('T')[0];
 
 function App() {
     const toDoInput = useRef();
     const dueDateInput = useRef();
     const [todos, setToDos] = useState([]);
+    const [sortConfig, setSortConfig] = useState({});
 
     useEffect(() => {
-        const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+        const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_DATA));
         setToDos(storedTodos)
     }, []); // run and clean effect only once on mount and unmount, this effect doesnt depend on any props or state so it never re-runs
-    //we only call this retrieving from localStorage once, because we only need to load from saved items when page first loads up, never again
+    // we only call this retrieving from localStorage once, because we only need to load from saved items when page first loads up, never again
 
     useEffect(() => {
-        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+        localStorage.setItem(LOCAL_STORAGE_KEY_DATA, JSON.stringify(todos))
     },[todos]); // this effect only fires if the todos array changes
 
     function handleAddItem(e) {
@@ -46,20 +48,63 @@ function App() {
     function handleClearItems() {
         const currentTodos = [...todos];
         const toRemainItems = currentTodos.filter(eachCurrentTodo => !eachCurrentTodo.completed);
+        console.log(toRemainItems, 'toRemainItems')
         setToDos(toRemainItems);
     }
 
+    function sortBy(e) {
+        const colToSort = e.target.value;
+        const currentSort = sortConfig;
+        const currentTodos = [...todos];
+        let direction;
+
+        if(!currentSort[colToSort]){
+            console.log(currentSort, currentSort[colToSort])
+            direction = 'asc';
+        }
+        else {
+            direction = currentSort[colToSort] === 'asc'? 'desc' : 'asc'; 
+        }
+        currentSort[colToSort] = direction;
+        setSortConfig(currentSort);
+
+        if(currentTodos) {
+            if(colToSort === 'date') {
+                currentTodos.sort((a,b) => {
+                    if(+(new Date(a.due)) < +(new Date(b.due))) return direction === 'asc'? -1 : 1;
+                    if(+(new Date(a.due)) > +(new Date(b.due))) return direction === 'asc'? 1 : -1;
+                    return 0;
+                });
+            }
+            if(colToSort === 'task') {
+                currentTodos.sort((a,b) => {
+                    if(a.name > b.name) return direction === 'asc'? -1 : 1;
+                    if(a.name < b.name) return direction === 'asc'? 1 : -1;
+                    return 0;
+                });
+            }
+            setToDos(currentTodos);
+        }
+    }
     return(
         <div>
             <h1>A To Do List</h1>
             <table>
                 <thead>
                     <tr>
-                        <th>Completion</th>
-                        <th>Task</th>
-                        <th>Due Date</th>
+                        <th>
+                            Completion
+                        </th>
+                        <th> 
+                            <button type="button" value='task' onClick={sortBy}>Task</button>
+                        </th>
+                        
+                        <th>
+                            <button type="button" value='date' onClick={sortBy}>Due Date</button>
+                        </th>
                     </tr>
                 </thead>  
+
                 <tbody>  
                     <ToDoItems todos={todos} toggleCompleted={toggleCompleted} />
                 </tbody>  
@@ -67,7 +112,6 @@ function App() {
             <input type='text' placeholder='event name'ref={toDoInput} />
             <input type="date" min={tomrISO} ref={dueDateInput}/> <button onClick={handleAddItem}> Add </button>
             <hr></hr>
-
             <button onClick={handleClearItems}> Clear Selected Items </button>
             <h3>Number of remaining to-do items: {todos.length}</h3>
         </div>
